24949796fcd6bd875c0ba1bfe2e35f95b06f0e3d
SamuelCr99 <samuel.collier.ryder@gmail.com>
2021-11-10 19:12:44 +0100

    Added 3 more sorting algos

--- main.py
+++ main.py
@@ -1,14 +1,52 @@
 import pygame
+import random
+from sorting_algo import *
 
-WIDTH, HEIGHT = 500,500
+WIDTH, HEIGHT = 800,800 #Height and width of the pygame window. 
+BLOCK_WIDTH = 40
 WIN = pygame.display.set_mode ((WIDTH,HEIGHT))
 
+class Block:
+    def __init__(self, height, color):
+        self.height = height
+        self.color = color
+
+#Draws the blocks and background onto the screen. 
+def draw_screen(win, block_list):
+    win.fill((255,255,255))
+
+    for x in range(len(block_list)):
+        block = pygame.Rect(x * BLOCK_WIDTH, HEIGHT - block_list[x].height, BLOCK_WIDTH, block_list[x].height)
+        color = block_list[x].color
+        pygame.draw.rect(WIN, color, block)
+    pygame.display.update()
+
+def check_quit():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            quit()   
+
+#Creates our blocks in an unsorted way. 
+def create_blocks():
+    amount_of_blocks = int(WIDTH / BLOCK_WIDTH)
+    block_list = []
+    for x in range(amount_of_blocks):
+        height = random.randint(0, HEIGHT)
+        red = random.randint(0, 255)
+        green = random.randint(0, 255)
+        blue = random.randint(0, 255)
+        color = (red, green, blue)
+        block_list.append(Block(height, color))
+    return block_list
+
 def main():
+    block_list = create_blocks()
     while(1):
-        WIN.fill((255,255,255))
-        pygame.display.update()
-        draw
+        #selection_sort(block_list,WIN)
+        #bubble_sort(block_list,WIN)
+        #insertion_sort(block_list,WIN)
+        
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
---
+++ sorting_algo.py
@@ -0,0 +1,45 @@
+#This is the file for the sorting algorithms and the smaller helper functions. 
+from main import *
+DELAY = 50
+
+def find_smallest(array):
+    smallest = array[0]
+    for i in range (1,len(array)):
+        if array[i] < smallest:
+            smallest = array[i]
+    return smallest
+
+def selection_sort(array, win):
+    for i in range(len(array)):
+        smallest = i
+        for k in range(i+1,len(array)):
+            if array[k].height < array[smallest].height:
+                smallest = k
+        tmp = array[i]
+        array[i] = array[smallest]
+        array[smallest] = tmp
+        draw_screen(win,array)
+        check_quit()
+        pygame.time.wait(DELAY)
+    return array
+
+def bubble_sort(arr, win):  # Completes the bubble sort algorithm to order the list.
+    for i in range(len(arr)-1):
+        for k in range(len(arr) - 1 - i):
+            check_quit()
+            if arr[k].height > arr[k + 1].height:
+                arr[k], arr[k + 1] = arr[k+1], arr[k]
+                draw_screen(win, arr)
+                pygame.time.delay(DELAY)
+    return arr
+
+def insertion_sort(arr, win):
+    for i in range(len(arr)):
+            j = i
+            while (j > 0 and arr[j].height < arr[j-1].height):
+                temp = arr[j]
+                arr[j] = arr[j-1]
+                arr[j-1] = temp
+                j -= 1
+                draw_screen(win, arr)
+                pygame.time.delay(DELAY) 
