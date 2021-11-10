a31209c07884787c27486037ca77cdee13e1b749
SamuelCr99 <samuel.collier.ryder@gmail.com>
2021-11-10 09:55:07 +0100

    First pygame stuff

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Hello great python")
+import pygame
+
+WIDTH, HEIGHT = 500,500
+WIN = pygame.display.set_mode ((WIDTH,HEIGHT))
+
+def main():
+    while(1):
+        WIN.fill((255,255,255))
+        pygame.display.update()
+        draw
+
+
+if __name__ == "__main__":
+    main()
