35cb54f0b3569558e944dca204f7b8c8acca185b
SamuelCr99 <samuel.collier.ryder@gmail.com>
2021-11-18 22:27:41 +0100

    Added better colors

--- visuals.py
+++ visuals.py
@@ -9,13 +9,10 @@ BLOCK_WIDTH = 1
 WIN = pygame.display.set_mode ((WIDTH,HEIGHT))
 
 
-FONT_SIZE = 20
+FONT_SIZE = 25
 PHASE_FONT = pygame.font.SysFont('ariel', FONT_SIZE)
 
-name_list = ["Selection sort", "Bubble sort", "Insertion sort", "Quick sort", "Bogo sort", "Something"]
-
-
-array_accesses = 0
+name_list = ["Selection sort", "Bubble sort", "Insertion sort", "Quick sort", "Bogo sort"]
 
 class Block:
     def __init__(self, height, color):
@@ -37,60 +34,85 @@ def draw_screen(win, block_list,array_accesses):
         color = block_list[x].color
         pygame.draw.rect(WIN, color, block)
     font = PHASE_FONT.render("Number of array accessed: " + str(array_accesses), 1, (0,0,0))
-    WIN.blit(font, (0, 10))
+    WIN.blit(font, (0, 5))
     pygame.display.update()
 
 def check_quit():
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            quit()   
+            quit()
 
 #Creates our blocks in an unsorted array of blocks, which will then be sorted.  
 def create_blocks():
     amount_of_blocks = int(WIDTH / BLOCK_WIDTH)
     block_list = []
     for x in range(amount_of_blocks):
-        height = random.randint(0, HEIGHT)
-        red    = random.randint(0, 255)
-        green  = random.randint(0, 255)
-        blue   = random.randint(0, 255)
+        height = random.randrange(0,HEIGHT)
+        red    = 255*(height/HEIGHT)#random.randint(0, 255)
+        green  = 60
+        blue   = 255*(1-height/HEIGHT) #random.randint(0, 255)
         color  = (red, green, blue)
         block_list.append(Block(height, (color)))
     return block_list
 
-def menu(win):
-    button_list = []
-    for i in range(3):
-        button_lower = pygame.Rect(i*350+50,200,200,100)
-        button_upper = pygame.Rect(i*350+50,600,200,100)
-        button_list.append(button_lower)
-        button_list.append(button_upper)
-    
-
-    win.fill((0,0,0))
-    for i in range(len(button_list)):
-        pygame.draw.rect(WIN, (255,0,0), button_list[i])
-        font = PHASE_FONT.render(name_list[i], 1, (255,255,255))
-        WIN.blit(font, (button_list[i].x+62, button_list[i].y+25))
+def menu(block_list,win):
+    win.fill((255,255,255))
+    font = PHASE_FONT.render("Press:", 1, (0,0,0))
+    win.blit(font, (5, 0))
+    for i in range(len(name_list)):
+        font = PHASE_FONT.render(str(i+1) + ". " + name_list[i], 1, (0,0,0))
+        win.blit(font, (5, (i+1)*20))
 
     while(1):
         pygame.display.update()
-        check_quit()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                quit() 
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_1:
+                    selection_sort(block_list,win)
+                elif event.key == pygame.K_2:
+                    bubble_sort(block_list,win)  
+                elif event.key == pygame.K_3:
+                    insertion_sort(block_list,win)  
+                elif event.key == pygame.K_4:
+                    quick_sort(block_list,win)  
+                elif event.key == pygame.K_5:
+                    bogo_sort(block_list,win)
+                return
+                               
 
 
-def main():
-    #menu(WIN)  
+def main(win):
     block_list = create_blocks()
-    #selection_sort(block_list,WIN)
-    #bubble_sort(block_list,WIN)
-    #insertion_sort(block_list,WIN)
-    quick_sort(block_list,WIN)
-    #bogo_sort(block_list,WIN)
+    menu(block_list,win)  
+    font = PHASE_FONT.render("Press R to restart", 1, (0,0,0))
+    win.blit(font, (0, 20))
+    pygame.display.update()
+
 
     while(1):
-        check_quit()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                quit() 
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_r:
+                    main(win) 
         
 
 
 if __name__ == '__main__':
-    main()
+    main(WIN) 
+
+
+
+
+
+
+
+
+
+
+
+
+ 
